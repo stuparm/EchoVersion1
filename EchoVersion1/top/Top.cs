2ff8c3f9e7fdb96d78fc8126863e12fae634afd3
stuparmihailo <stupar.mih@gmail.com>
2014-10-04 02:13:54 +0200

    Komit1

---
+++ EchoVersion1/top/Top.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoVersion1.top
+{
+    public class Top
+    {
+        private static Top instance;
+
+        private static Top()
+        {
+
+        }
+
+
+        public Top Instance
+        {
+            get 
+            {
+                if (instance == null)
+                    instance = new Top();
+                return instance; 
+            }
+            
+        }
+
+        public void biloSta()
+        {
+
+        }
+        
+
+
+
+    }
+}
